bc6d534cbc120e376e92cb0489a6ec745e7acc4b
David Barbet <dabarbet@microsoft.com>
2024-11-19 17:24:02 -0800

    working...

---
+++ MSBuildWorkspaceRunner/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+
+Console.WriteLine("Hello, World!");
+
+MSBuildLocator.RegisterDefaults();
+
+var workspace = MSBuildWorkspace.Create();
+
+workspace.WorkspaceFailed += (sender, args) =>
+{
+    Console.WriteLine(args.Diagnostic.Message);
+};
+
+var slnPath = args[0];
+Console.WriteLine($"Opening solution {slnPath}");
+
+var solution = await workspace.OpenSolutionAsync(slnPath, new Progress());
+
+foreach(var project in solution.Projects)
+{
+    Console.WriteLine($"Project {project.Name} has {project.Documents.Count()} documents");
+}
+
+
+class Progress : IProgress<ProjectLoadProgress>
+{
+    public void Report(ProjectLoadProgress value)
+    {
+        Console.WriteLine($"{value.Operation} completed for {value.FilePath} ({value.TargetFramework}) in {value.ElapsedTime}ms");
+    }
+}   
